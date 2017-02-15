a6fbfca316d51895e2c571d4a104095c08dce79e
Damian Łukasik <Damianlukasik@mail.com>
2017-02-15 10:03:41 +0100

    first commit

---
+++ Stos_Console/Stos_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Stos_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack liczby = new Stack();
+
+            liczby.Push(10);
+            liczby.Push(11);
+            liczby.Push(2);
+            liczby.Push(7);
+
+            foreach(int liczba in liczby)
+            {
+                Console.WriteLine(liczba);
+            }
+
+            while (liczby.Count != 0)
+            {
+                int liczba = (int)liczby.Pop();
+                Console.WriteLine(liczba+" została usunięta ze stosu");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
