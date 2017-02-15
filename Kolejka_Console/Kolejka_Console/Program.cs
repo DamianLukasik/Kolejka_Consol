83e429cb43c310fc7cf221d8e2db8d4cc20d7f04
Damian Łukasik <Damianlukasik@mail.com>
2017-02-15 09:45:44 +0100

    First commit

---
+++ Kolejka_Console/Kolejka_Console/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+
+namespace Kolejka_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue liczby = new Queue();
+            Console.WriteLine("\n< kładanie na kolejkę >\n");
+            foreach (int liczba in new int[4] { 9, 3, 7, 2 })
+            {
+                liczby.Enqueue(liczba);
+                Console.WriteLine("\t" + liczba + " dodana do kolejnki");
+            }
+            Console.WriteLine("\n< zawartość kolejki >\n");
+            foreach (int liczba in liczby)
+            {
+                Console.WriteLine("\t" + liczba);
+            }
+            Console.WriteLine("\n< zdejmowanie z kolejki >\n");
+            while (liczby.Count != 0)
+            {
+                int liczba = (int)liczby.Dequeue();
+                Console.WriteLine("\t" + liczba + " odjęta z kolejki");
+            }
+            Console.ReadKey();
+        }
+    }
+}
